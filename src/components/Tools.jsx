7c3bd4b8abafdc7bf7b0916a1f14c04fbd5920d8
Danil Zobkov <56187773+Owelve@users.noreply.github.com>
2024-09-02 19:36:20 +0300

    Update Tools.jsx

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -5,7 +5,7 @@ const Tools = () => {
     <div className='w-full flex flex-col items-center gap-[70px]'>
       <h2 className='xl:text-[40px] md:text-[32px] msm:text-[28px] msm:text-center'><span className='text-blue'>Технологии</span> которые я использую</h2>
       <ul className='flex flex-row flex-wrap gap-x-[50px] xl:mb-[20px] xl:text-[36px] justify-center'>
-        <li><a href="https://react.dev/">React</a></li>
+        <li><a href="https://nextjs.org/">NextJS</a></li>
         <li><a href="https://www.typescriptlang.org/">Typescript</a></li>
         <li><a href="https://go.dev/">Golang</a></li>
         <li><a href="https://tailwindcss.com/">TailWindCSS</a></li>
@@ -14,9 +14,9 @@ const Tools = () => {
         <li><a href="https://nginx.org/ru/">Nginx</a></li>
         <li><a href="https://httpd.apache.org/">Apache</a></li>
         <li><a href="https://memcached.org/">Memcached</a></li>
-        <li><a href="https://www.php.net/">PHP</a></li>
+        <li><a href="https://www.php.net/">php</a></li>
         <li><a href="https://kafka.apache.org/">Kafka</a></li>
-        <li><a href="https://www.docker.com/">Doker</a></li>
+        <li><a href="https://www.docker.com/">Docker</a></li>
         <li><a href="https://gin-gonic.com/">Gin</a></li>
       </ul>
     </div>
